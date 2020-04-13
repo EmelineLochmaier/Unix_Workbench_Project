81fdaf322060ff258a34a2bfbb6d4c67ad15038c
EmelineLochmaier <emma@lochbox.com>
2020-04-13 09:32:30 -0400

    modified guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,17 +2,17 @@ echo "Let's play a game.... How many files are in this repository?"
 
 read $1
 
-filecount=git ls-files | wc -l
+file_count=git ls-files | wc -l
 
-while [[ $1 -ne $filecount ]] 
+while [[ $1 -ne $file_count ]] 
 do
-	if [[ $1 gt $filecount ]]
+	if [[ $1 gt $file_count ]]
 	then
 		echo "Nope. Too large. Try again."
 		echo "How many files are in this repository?"
 		read $1
 	fi
-	if [[ $1 lt $filecount ]]
+	if [[ $1 lt $file_count ]]
 	then
 		echo "Nope. Too small. Try again."
 		echo "How many files are in this repository?"
@@ -20,7 +20,7 @@ do
 	fi
 done
 
-if [[ $1 -eq $filecount ]]
+if [[ $1 -eq $file_count ]]
 then
 	echo "Congrats you survived!"
 fi
